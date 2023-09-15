b78f3ff96175ba7d8c95f39c55ac22a97bac85c3
zan557 <angzq0037@gmail.com>
2023-09-15 18:01:06 +0800

    Added routes and handler framework

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const wiki = require("./wiki.js");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 const app = express();
 
@@ -37,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/wiki", wiki);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
